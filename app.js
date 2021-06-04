f5deada13d2524f7ca26355342b677baafeb7a3e
Rodion Sibov <rodionsibov@gmail.com>
2021-06-04 13:17:26 +0300

    feat: place the player ang goal sprites

--- app.js
+++ app.js
@@ -57,11 +57,25 @@ class Game {
         map.style.height = this.map.length * this.tileDim + 'px'
         map.style.width = this.map[0].length * this.tileDim + 'px'
     }
+
+    placeSprite(type) {
+        let x = this[type].x
+        let y = this[type].y
+        let sprite = this.createEl(x, y, type)
+        sprite.id = type
+        sprite.style.borderRadius = this.tileDim + 'px'
+        let layer = this.el.querySelector('.sprites')
+        layer.appendChild(sprite)
+        return sprite
+    }
 }
 
 function init() {
     let myGame = new Game('.game-container-1')
     myGame.populateMap()
     myGame.sizeUp()
+    myGame.placeSprite('goal')
+    let playerSprite = myGame.placeSprite('player')
+    myGame.player.el = playerSprite
 }
 init()
